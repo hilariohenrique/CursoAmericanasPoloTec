3a7d50b1b09c3a2f3190ad821c98ddec13bf6164
hilariohenrique <hilariohenr@hotmail.com>
2022-12-17 14:48:04 -0300

    Criação da classe Jogadores para cadastrar os jogadores como objetos.

--- src/BE_JV_001_LDP01/ProjetoModulo/JogoDaVelha.java
+++ src/BE_JV_001_LDP01/ProjetoModulo/JogoDaVelha.java
@@ -1,5 +1,26 @@
 package BE_JV_001_LDP01.ProjetoModulo;
 
+import java.util.Scanner;
+
 public class JogoDaVelha {
+    public static void main(String[] args) {
+        jogoDaVelha();
+    }
+
+    private static void jogoDaVelha() {
+
+    }
+
+    class Jogadores {
+        String nome;
+        boolean vencedor;
+
+        public Jogadores(String nome, boolean vencedor) {
+            this.nome = nome;
+            this.vencedor = vencedor;
+        }
+
+
+    }
 
 }
