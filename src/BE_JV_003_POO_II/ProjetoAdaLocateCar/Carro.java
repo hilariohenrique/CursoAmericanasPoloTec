c8f8614780385cdb64062a1c9b8f3ff9d653695d
hilariohenrique <hilariohenr@hotmail.com>
2023-02-13 17:16:32 -0300

    Projeto aluguel de veículos

--- src/BE_JV_003_POO_II/ProjetoAdaLocateCar/Carro.java
+++ src/BE_JV_003_POO_II/ProjetoAdaLocateCar/Carro.java
@@ -3,18 +3,16 @@ package BE_JV_003_POO_II.ProjetoAdaLocateCar;
 import java.util.List;
 
 public class Carro implements Veiculo {
-    private final String placa;
-    private final String nome;
-    private final TipoVeiculo tipo;
+    private String placa;
+    private String nome;
+    private TipoVeiculo tipo;
     private boolean alugado;
 
     private List<Veiculo> listaVeiculos;
 
     public Carro(List<Veiculo> listaVeiculos, String placa, String nome, TipoVeiculo tipo) {
-        for (Veiculo veiculo : listaVeiculos) {
-            if (veiculo.getPlaca() == placa) {
-                throw new RuntimeException("O veículo de placa " + placa + " já foi cadastrado, favor cadastrar outro veículo");
-            }
+        if (carroExiste(listaVeiculos, placa)) {
+            throw new RuntimeException("O veículo de placa " + placa + " já foi cadastrado, favor cadastrar outro veículo");
         }
         this.placa = placa;
         this.nome = nome;
@@ -22,21 +20,45 @@ public class Carro implements Veiculo {
         this.alugado = false;
     }
 
+    @Override
     public String getPlaca() {
         return placa;
     }
 
+    @Override
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
     public String getNome() {
         return nome;
     }
 
     @Override
-    public void editar() {
-
+    public void setNome(String nome) {
+        this.nome = nome;
     }
+
 
     public TipoVeiculo getTipo() {
         return this.tipo;
+    }
+
+    @Override
+    public void setTipo(TipoVeiculo tipoVeiculo) {
+        this.tipo = tipoVeiculo;
+    }
+
+    private boolean carroExiste(List<Veiculo> listaVeiculos, String placa) {
+        if (!listaVeiculos.isEmpty()) {
+            for (Veiculo veiculo : listaVeiculos) {
+                if (veiculo.getPlaca() == placa) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
@@ -53,6 +75,11 @@ public class Carro implements Veiculo {
 
     public boolean isAlugado() {
         return alugado;
+    }
+
+    @Override
+    public void setAlugado() {
+
     }
 
     public void setAlugado(boolean alugado) {
--- src/BE_JV_003_POO_II/ProjetoAdaLocateCar/Veiculo.java
+++ src/BE_JV_003_POO_II/ProjetoAdaLocateCar/Veiculo.java
@@ -2,16 +2,17 @@ package BE_JV_003_POO_II.ProjetoAdaLocateCar;
 
 public interface Veiculo {
     String getPlaca();
+    void setPlaca(String placa);
 
     String getNome();
-
-    void editar();
+    void setNome(String nome);
 
     TipoVeiculo getTipo();
+    void setTipo(TipoVeiculo tipoVeiculo);
 
     boolean isAlugado();
 
-    void setAlugado(boolean alugado);
+    void setAlugado();
 
     double getValorDiaria();
 }
